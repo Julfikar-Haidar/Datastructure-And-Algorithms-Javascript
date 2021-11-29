40276e83e9ec59531ced10102a2b1a5a5f7314db
Julfikar Haidar <julfikar6262@gmail.com>
2021-11-29 10:58:28 +0600

    binary seartch

---
+++ binary_search.js
@@ -0,0 +1,21 @@
+function binarySearch(arr,element) {
+    let start = 0;
+    let end = arr.length -1;
+    let middle = Math.floor((start + end) / 2);
+    while (arr[middle] !== element && start <= end) {
+        if (element < arr[middle]) {
+            end = middle -1
+        } else {
+            start = middle + 1
+        }
+        middle = Math.floor((start + end) / 2)
+    }
+
+    if(arr[middle] === element) {
+        return middle
+    }
+    return -1
+
+}
+
+binarySearch([2, 5, 6, 9, 13, 15, 28, 30], 103)
